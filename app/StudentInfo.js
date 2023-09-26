2c097a01b9756fb88deb30adc232caecd981ac07
ZeusEstrella <zeusaixcel31@gmail.com>
2023-09-25 21:26:14 -0600

    week2

---
+++ app/StudentInfo.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+
+function StudentInfo() {
+    return (
+      <div>
+        <h1>CPRG 306: Web Development 2 - Assignments</h1>
+        <h2>Your Name: Zeus Aixcel Estrella</h2>
+        <p>Course section: CPRG 306 A/B/C/D/...</p>
+        <p>
+          <Link href="https://github.com/ZeusEstrella/cprg306-assignments">
+          </Link>
+        </p>
+      </div>
+    );
+  }
+  
+  export default StudentInfo;
+
+  
---
+++ app/week2/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+import StudentInfo from './/StudentInfo.js'; 
+
+function Week2Page() {
+  return (
+    <div>
+      <h1>My Shopping List</h1> 
+      <StudentInfo /> 
+    </div>
+  );
+}
+
+export default Week2Page;
