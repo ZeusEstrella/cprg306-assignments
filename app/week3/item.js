c0990c7cfaec274ffbfaf50d93a192c58a85d7c0
ZeusEstrella <zeusaixcel31@gmail.com>
2023-10-01 10:10:54 -0600

    week3

---
+++ app/week3/item.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const item = ({ name, quantity, category }) => {
+  return (
+    <li className="bg-gray-100 rounded p-2 my-2">
+      <div className="text-lg font-semibold">{name}</div>
+      <div className="text-sm text-gray-500">Category: {category}</div>
+      <div className="text-sm text-gray-500">Quantity: {quantity}</div>
+    </li>
+  );
+};
+
+export default item;
